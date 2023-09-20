6fcd5f9f71f3b95039373b96ea1ef5fe9f585a5f
Matthew Thibodeau <matt.thibodeau@rice.edu>
2023-09-20 11:54:25 -0500

    Create ttn_perform_qlearn.py

---
+++ ttn_learning/ttn_perform_qlearn.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 20 11:22:21 2023
+
+@author: matthewthibodeau
+"""
+import numpy as np
+import sys
+
+sys.path.insert(0,'../ttn_demo')
+
+import ttn
+import ttn_q_learn
+
+...
